2aa3c2e9bb8fa3c53273ba5358820b3c932c1a39
Joshua_Ajagbe <joshua@equally.ai>
2024-02-03 06:49:53 +0100

    user model

---
+++ models/user.go
@@ -0,0 +1,16 @@
+package models
+
+type User struct {
+	FirstName	string
+	LastName		string
+	Tagline		string
+	Country		string
+	ZipCode		string
+	Email			string
+	Profession	string
+	Income		uint32
+	Company		string
+	Links			string
+	UserID		uint32
+	Followers	uint32
+}
